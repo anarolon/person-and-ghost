0b0a7b114fd36242908ecf6094e7cd2550c13406
A4Michael <michael.delgado1@upr.edu>
2021-09-24 18:03:32 -0400

    Level 1-1 Base Implementation
    Core gameplay was implemented. Need to add the goal object, winning UI, dying or unpossession when falling.

--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -3,13 +3,24 @@ using UnityEngine.InputSystem;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    [Header("Players Fields")]
-    [SerializeField] private GameObject _personPrefab = default;
-    [SerializeField] private GameObject _ghostPrefab = default;
+    [Header("Player 1 Fields")]
+    [SerializeField] private GameObject _player1Prefab = default;
+    [SerializeField] private Vector2 _player1Position = Vector2.one;
+
+    [Header("Player 2 Fields")]
+    [SerializeField] private GameObject _player2Prefab = default;
+    [SerializeField] private Vector2 _player2Position = -Vector2.one;
 
     private void Start()
     {
-        PlayerInput.Instantiate(_personPrefab, controlScheme:"KeyboardLeft", pairWithDevice: Keyboard.current);
-        PlayerInput.Instantiate(_ghostPrefab, controlScheme:"KeyboardRight", pairWithDevice: Keyboard.current);
+        var player1PlayerInput = PlayerInput.Instantiate(_player1Prefab,
+                                                       controlScheme: "KeyboardLeft",
+                                                       pairWithDevice: Keyboard.current);
+        var player2PlayerInput = PlayerInput.Instantiate(_player2Prefab, 
+                                                       controlScheme: "KeyboardRight", 
+                                                       pairWithDevice: Keyboard.current);
+
+        player1PlayerInput.gameObject.transform.position = _player1Position;
+        player2PlayerInput.gameObject.transform.position = _player2Position;
     }
 }
