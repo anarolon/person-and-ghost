6155f589a95da89f0fe38bc62a9cdc8bf6dcdf21
A4Michael <michael.delgado1@upr.edu>
2021-09-29 17:26:52 -0400

    DEV#35 | Implement Collectables Counter
    # Changes
    - Added Collectables Amount Text
    - Added Collectables Image
    - Added Tool Container Image
    - Added Options Button
    - Added Spirit Slider
    - Implemented Collectable Amount Text Update in PersonCollectableController
    
    # To-do
    - Polish Code

--- Assets/Scripts/Person/PersonCollectableController.cs
+++ Assets/Scripts/Person/PersonCollectableController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using PersonAndGhost;
 
 public class PersonCollectableController : MonoBehaviour
 {
     [SerializeField] private int collectableCount = 0;
+    private Text _textBox = default;
+
     // Start is called before the first frame update
     void Start()
     {
         collectableCount = 0;
+        _textBox = FindObjectOfType<Canvas>().GetComponentsInChildren<Text>()[1];
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,6 +22,7 @@ public class PersonCollectableController : MonoBehaviour
         {
             Collectable collectable = collision.gameObject.GetComponent<Collectable>();
             collectableCount += collectable.worth;
+            _textBox.text = collectableCount.ToString();
             Destroy(collision.gameObject);
         }
     }
