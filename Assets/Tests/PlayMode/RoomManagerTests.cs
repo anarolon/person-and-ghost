48034895d9292463c81dbb783ac6afca322cd778
A4Michael <michael.delgado1@upr.edu>
2021-10-27 00:22:21 -0400

    DEV #68 | Room Manager Finished
    ## Changes
    - Added Room Manager functionality as specified in card to the GameManagerController.
    - Modified the Actions and Utility files to support the new functionality.
    - Modified EditorBuildSettings, Level 1-1, ContactKiller, Goal, and PlayerTracker to support the newly enhanced functionality.

---
+++ Assets/Tests/PlayMode/RoomManagerTests.cs
@@ -0,0 +1,75 @@
+using UnityEngine.TestTools;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using PersonAndGhost.Utils;
+using UnityEngine.SceneManagement;
+
+namespace PersonAndGhost.PlayMode
+{
+    public class RoomManagerTests
+    {
+        private int SceneAmount => SceneManager.sceneCountInBuildSettings - 1;
+        private string FirstSceneName => SceneManager.GetSceneByBuildIndex(0).name;
+        private string CurrentSceneName => SceneManager.GetActiveScene().name;
+        private string LastSceneName => 
+            SceneManager.GetSceneByBuildIndex(SceneAmount - 1).name;
+
+        [UnityTest]
+        public IEnumerator TransitionFromFirstSceneToLast()
+        {
+            yield return TestTransitionFromFirstSceneToItself();
+
+            if (SceneAmount > 1)
+            {
+                yield return TestTransitionToNextScene();
+            }
+
+            yield return TestTransitionFromLastSceneToFloorWin();
+        }
+
+        private IEnumerator TestTransitionFromFirstSceneToItself()
+        {
+            SceneManager.LoadSceneAsync(0);
+
+            yield return new WaitUntil(() => CurrentSceneName.Contains(FirstSceneName));
+
+            yield return SceneTransitionHelper(false);
+
+            Assert.AreEqual(FirstSceneName, CurrentSceneName);
+        }
+
+        private IEnumerator TestTransitionToNextScene()
+        {
+            yield return SceneTransitionHelper(true);
+
+            Assert.AreNotEqual(FirstSceneName, CurrentSceneName);
+        }
+
+        private IEnumerator TestTransitionFromLastSceneToFloorWin()
+        {
+            SceneManager.LoadSceneAsync(SceneAmount - 1);
+
+            yield return new WaitUntil(() => CurrentSceneName.Contains(LastSceneName));
+
+            Assert.NotZero(Time.timeScale);
+
+            Actions.OnFloorStateChange(true);
+
+            yield return new WaitForSecondsRealtime(
+                Object.FindObjectOfType<GameManagerController>()
+                .timeToWaitBeforeLoadingScene + 1);
+
+            Assert.Zero(Time.timeScale);
+        }
+
+        private static IEnumerator SceneTransitionHelper(bool state)
+        {
+            Actions.OnRoomStateChange(state);
+
+            yield return new WaitForSecondsRealtime(
+                Object.FindObjectOfType<GameManagerController>()
+                .timeToWaitBeforeLoadingScene + 1);
+        }
+    }
+}
