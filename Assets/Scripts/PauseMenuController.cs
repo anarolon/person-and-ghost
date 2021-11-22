7008adb98e29c0563a3537faca6bcebff3ea15f1
A4Michael <michael.delgado1@upr.edu>
2021-11-22 12:34:30 -0400

    DEV #171 | Finished!
    ## Changes
    - Add Spirit death sound
    - Add UIMoving and UISelecting sound effects support

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -2,6 +2,7 @@ using PersonAndGhost.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace PersonAndGhost
 {
@@ -10,10 +11,15 @@ namespace PersonAndGhost
         private GameObject _pauseMenuPanel;
         // TODO: Code a way to make sure _pauseFirstButton is set to an object
         [SerializeField] private GameObject _pauseFirstButton;
+        
+        private Button _currentButton = default;
+        private GameObject _previousSelected = default;
+        private EventSystem _eventSystem;
 
         private void Start()
         {
             _pauseMenuPanel = transform.GetChild(0).gameObject;
+            _eventSystem = EventSystem.current;
         }
 
         private void OnEnable()
@@ -28,16 +34,52 @@ namespace PersonAndGhost
             Actions.OnGamePause -= HandleGamePause;
         }
 
+        private void Update()
+        {
+            GameObject currentSelected = _eventSystem.currentSelectedGameObject;
+
+            if (_pauseMenuPanel.activeSelf)
+            {
+                if (_previousSelected == currentSelected)
+                {
+                    if (!_currentButton)
+                    {
+                        // The following assignment may slow performance because 
+                        //  it is called each time a new button is selected.
+                        _currentButton = currentSelected.GetComponent<Button>();
+                    }
+                }
+
+                else
+                { 
+                    Utility.ActionHandler(Actions.Names.OnRequestAudio, Clips.UIMoving, this);
+
+                    _previousSelected = currentSelected;
+                    _currentButton = null;
+                }
+
+                if (_currentButton)
+                {
+                    _currentButton.onClick.AddListener(delegate
+                    {
+                        Utility.ActionHandler(Actions.Names.OnRequestAudio, Clips.UISelecting, this);
+                    });
+                }
+            }
+        }
+
         private void HandleGamePause()
         {
             if (_pauseMenuPanel && !_pauseMenuPanel.activeSelf)
             {
-                if (EventSystem.current)
+                if (_eventSystem)
                 {
-                    EventSystem.current.SetSelectedGameObject(null);
+                    _eventSystem.SetSelectedGameObject(null);
                     if (_pauseFirstButton)
                     {
-                        EventSystem.current.SetSelectedGameObject(_pauseFirstButton);
+                        _eventSystem.SetSelectedGameObject(_pauseFirstButton);
+
+                        _previousSelected = _eventSystem.currentSelectedGameObject;
                     }
                 }
             
